2af6095553b6436027aeba45bf29afc6cde647fb
ian.lab(jaewon.lee)/melon <ian.lab@kakaoent.com>
2024-06-06 16:52:23 +0900

    서비스 도메인 (www.voclab.co.kr) 과 API 도메인 (api.voclab.co.kr) 분리로 인해 cors 오류 수정

--- voc-kmac-main/src/main/java/kr/co/kmac/configuration/WebMvcConfig.java
+++ voc-kmac-main/src/main/java/kr/co/kmac/configuration/WebMvcConfig.java
@@ -3,6 +3,7 @@ package kr.co.kmac.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -63,4 +64,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 ;
 
     }
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+	}
 }
