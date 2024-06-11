6070238efe1280f47b2d3ce283e2472c14586759
ian.lab(jaewon.lee)/melon <ian.lab@kakaoent.com>
2024-06-12 01:58:12 +0900

    로컬 테스트가 안되서

--- voc-kmac-main/src/main/java/kr/co/kmac/configuration/WebMvcConfig.java
+++ voc-kmac-main/src/main/java/kr/co/kmac/configuration/WebMvcConfig.java
@@ -68,7 +68,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-				.allowedOrigins("http://www.voclab.co.kr", "http://api.voclab.co.kr", "http://m.voclab.co.kr")
+				.allowedOrigins("http://www.voclab.co.kr", "http://api.voclab.co.kr", "http://m.voclab.co.kr", "http://127.0.0.1:9000", "http://localhost:9000")
 				.allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
 				.exposedHeaders("Set-Cookie")
 				.allowCredentials(true);
